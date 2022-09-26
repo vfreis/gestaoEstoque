3472de80fc58a70b7395837132c2458b67aee8d0
vfreis <vinicios.falqueiro@gmail.com>
2022-09-25 23:43:31 -0300

    alteracao arq

--- views.py
+++
@@ -1,9 +0,0 @@
-from flask import (
-    render_template, request, Blueprint, session, redirect, url_for
-)
-
-views = Blueprint(__name__, 'views')
-
-@views.route('/')
-def index():
-    return render_template('index.html')
